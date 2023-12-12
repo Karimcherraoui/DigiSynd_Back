c96a9d3108a128d9866d757a3931672338485530
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-12 11:02:13 +0100

    Add JWT authentication middleware

---
+++ src/middleware/jwt.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const authenticateToken = (req, res, next) => {
+  const tokenWithBearer = req.headers.authorization;
+  if (!tokenWithBearer)
+    return res.status(401).json({ error: "Access denied" });
+  const [, token] = tokenWithBearer.split(" ");
+  
+  
+  try {
+
+   
+    if(token.length >= 500) {
+      const decoded = jwt.decode(token);
+      req.admin = {
+        adminId: decoded.sub,
+        fullName : decoded.fullName,
+        email: decoded.email,
+      };
+    } else {
+      const decoded = jwt.verify(token, process.env.SECRET_KEY);
+      req.admin = {
+        adminId: decoded.adminId,
+        email: decoded.email,
+        fullName : decoded.fullName,
+      };
+    }
+    next();
+  } catch (error) {
+    res.status(400).json({ error: "Invalid token" });
+  }
+};
