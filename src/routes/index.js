4af2e3658de412b5774c5ccf7a48a99c26b9fda9
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-12-19 22:30:53 +0100

    Add JWT authentication middleware to router

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ import AuthRoutes from "./authRoute"
 
 
 import { json } from "express"; 
+import { authenticateToken } from "../middleware/jwt";
 
 
 const router  =  Router();
 router.use(json())
 router.use("/admin", AuthRoutes);
+router.use(authenticateToken);
 router.use("/apartment" , apartmentRoute)
 
 export default router
